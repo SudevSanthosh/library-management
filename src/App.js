f438f09d1a467501c6640c5a57a076cd1fdfc129
Sudev S <sudev5552@gmail.com>
2023-02-15 14:56:27 +0530

    -routing setup -updated folder structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Page1 from "./pages/Page1";
+import Page2 from "./pages/Page2";
 
 function App() {
   return (
-    <div className="App">
-       <h1 class="text-3xl font-bold underline">Library Management</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Page1 />} />
+        <Route path="/page2" element={<Page2 />} />
+      </Routes>
+      <Link to="/" style={{ padding: 5 }}>
+        Home
+      </Link>
+      <Link to="/page2" style={{ padding: 5 }}>
+        Page 2
+      </Link>
+    </Router>
   );
 }
 
