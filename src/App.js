e8c12c285bd3a0da43cf8a7785b1ba78dd99d5f2
Sudev S <sudev5552@gmail.com>
2023-03-09 22:48:38 +0530

    -my profile for student login -route for my profile page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import WelcomePage from "./pages/WelcomePage";
 import StudentList from "./pages/StudentList";
+import MyProfile from "./pages/MyProfile";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LoginPage/>} />
-        <Route path="/welcomePage" element={<WelcomePage/>} />
-        <Route path="/studentList" element={<StudentList/>} />
-    </Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/welcomePage" element={<WelcomePage />} />
+        <Route path="/studentList" element={<StudentList />} />
+        <Route path="/myProfile" element={<MyProfile />} />
+      </Routes>
     </Router>
   );
 }
